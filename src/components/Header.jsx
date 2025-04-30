4e37a7acd6f336998cbc3de7f998a29eb8ae1498
kontsol <kostastsolakidhs20@gmail.com>
2025-04-30 17:58:42 +0300

    updated css

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -27,22 +27,27 @@ const Header = ({ onSearch }) => {
         className="block md:hidden absolute top-0 w-full h-full object-cover"
       />
       <div className="absolute w-full px-2 sm:px-4">
-        <h1 className="text-white text-center font-semibold text-md md:text-lg">
+        <h1 className="text-white text-center font-semibold text-sm md:text-md mb-1">
           IP Address Tracker
         </h1>
         <div className="flex justify-center">
           <input
             type="text"
             placeholder="Search for any IP address or domain"
-            className="pl-1 md:pl-5 bg-white outline-none text-black text-xs max-w-[700px] w-full rounded-l-lg"
+            className="pl-2 md:pl-5 bg-white outline-none text-black text-[0.5rem] md:text-xs max-w-[700px] w-full rounded-l-lg"
             onChange={(e) => setInputValue(e.target.value)}
             value={inputValue}
+            onKeyDown={(e) => {
+              if (e.key === "Enter") {
+                handleClick();
+              }
+            }}
           />
           <img
             onClick={handleClick}
             src={arrow}
             alt="search-bar-arrow"
-            className="bg-gray-800 hover:bg-gray-700 px-5 py-4 rounded-r-lg cursor-pointer transition-colors duration-300"
+            className="bg-gray-800 hover:bg-gray-700 px-3 md:px-5 py-3 md:py-4 rounded-r-lg cursor-pointer transition-colors duration-300"
           />
         </div>
       </div>
--- src/components/InfoCard.jsx
+++ src/components/InfoCard.jsx
@@ -8,39 +8,39 @@ const InfoCard = ({ ipData }) => {
       label: "IP Address",
       value: ipData.ip,
       padding: "md:pr-4",
-      border: "md:border-r-1",
+      border: "border-b-1 md:border-b-0 md:border-r-1",
     },
     {
       label: "Location",
       value: `${ipData.location.city}, ${ipData.location.region} ${ipData.location.postalCode}`,
       padding: "md:pr-4",
-      border: "md:border-r-1",
+      border: "border-b-1 md:border-b-0 md:border-r-1 ",
     },
     {
       label: "Timezone",
       value: `UTC ${ipData.location.timezone}`,
       padding: "md:pr-4",
-      border: "md:border-r-1",
+      border: "border-b-1 md:border-b-0 md:border-r-1",
     },
     {
-      label: "ISP",
-      value: ipData.isp,
+      label: "Country",
+      value: ipData.location.country,
       padding: "",
     },
   ];
 
   return (
-    <div className=" bg-white py-1 md:py-3 md:px-4 max-w-[1200px] w-full rounded-2xl shadow-4xl absolute z-100  -top-12 flex flex-col  md:flex-row left-1/2 -translate-x-1/2">
+    <div className=" bg-white py-2 md:py-4 md:px-4 max-w-[1200px] w-full rounded-2xl shadow-4xl absolute z-100  -top-17 md:-top-12 flex flex-col md:flex-row gap-2 left-1/2 -translate-x-1/2">
       {infoItems.map((item, index) => (
         <div
           key={index}
-          className={`flex-1 text-center md:text-start ${item.padding} `}
+          className={`px-6 md:px-0 flex-1 text-center md:text-start ${item.padding} `}
         >
-          <label className="text-[0.5rem] font-semibold text-gray-600 uppercase tracking-widest">
+          <label className="block text-[0.5rem] font-semibold text-gray-500 uppercase tracking-widest">
             {item.label}
           </label>
           <p
-            className={`text-xs font-semibold mt-1 h-6 md:h-12 border-gray-300 ${item.border}`}
+            className={`text-xs font-semibold md:mt-2 h-8 md:h-10 ${item.border} border-neutral-300 flex flex-col justify-center`}
           >
             {item.value}
           </p>
